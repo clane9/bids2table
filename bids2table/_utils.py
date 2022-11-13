8b3239e599d094a1a65de84294c42d30ab265731
Connor Lane <connor.lane858@gmail.com>
2022-11-13 14:18:25 -0500

    update `ParquetWriter` to automatically write out partitions

--- bids2table/_utils.py
+++ bids2table/_utils.py
@@ -1,6 +1,11 @@
 import fnmatch
 import logging
+import re
+from glob import glob
+from pathlib import Path
 from typing import Dict, Generic, Iterable, List, Optional, TypeVar
+
+import pandas as pd
 
 T = TypeVar("T")
 
@@ -48,6 +53,9 @@ class Catalog(Generic[T]):
         # NOTE: This is the only thing that differentiates a catalog from a dict
         # TODO: It might be possible to search more efficiently using a pandas str series:
         # https://pandas.pydata.org/docs/reference/api/pandas.Series.str.match.html
+        if set(pattern).isdisjoint("[]?*"):
+            obj = self.get(pattern)
+            return [] if obj is None else [obj]
         matching_keys = fnmatch.filter(self._catalog.keys(), pattern)
         return [self._catalog[k] for k in matching_keys]
 
@@ -68,3 +76,60 @@ def set_overlap(a: Iterable, b: Iterable) -> float:
     a, b = set(a), set(b)
     aintb = a.intersection(b)
     return len(aintb) / min(len(a), len(b), 1)
+
+
+def expand_paths(paths: List[str], recursive: bool = False) -> pd.Series:
+    """
+    Expand any glob patterns in `paths` and make paths absolute. Return expanded paths
+    as a pandas `Series`.
+    """
+
+    def abspath(path):
+        return str(Path(path).absolute())
+
+    expanded_paths = []
+    for path in paths:
+        if set(path).isdisjoint("[]*?"):
+            expanded_paths.append(abspath(path))
+        else:
+            matched_paths = sorted(glob(path, recursive=recursive))
+            expanded_paths.extend(map(abspath, matched_paths))
+
+    expanded_paths = pd.Series(expanded_paths, dtype=pd.StringDtype)
+    return expanded_paths
+
+
+def format_task_id(task_id: int) -> str:
+    """
+    Format a task ID as a zero-padded string.
+    """
+    return f"{task_id:05d}"
+
+
+def parse_size(size: str) -> int:
+    """
+    Parse a human readable size string like ``10MB`` to integer bytes.
+    """
+    units = {
+        "B": 1,
+        "KB": 10**3,
+        "MB": 10**6,
+        "GB": 10**9,
+        "KiB": 1024,
+        "MiB": 1024**2,
+        "GiB": 1024**3,
+    }
+    units_lower = {k.lower(): v for k, v in units.items()}
+
+    pattern = "(.*?)({units})".format(units="|".join(units_lower.keys()))
+    match = re.match(pattern, size, flags=re.IGNORECASE)
+    if match is None:
+        raise ValueError(
+            f"Size {size} didn't match any of the following units:\n\t"
+            + ", ".join(units.keys())
+        )
+    size = match.group(1)
+    num = float(size)
+    unit = match.group(2)
+    bytesize = int(num * units_lower[unit.lower()])
+    return bytesize
--- bids2table/writer.py
+++ bids2table/writer.py
@@ -1,10 +1,13 @@
 import logging
+import os
+from pathlib import Path
 from typing import Optional, Union
 
 import pandas as pd
 import pyarrow as pa
 from pyarrow import parquet as pq
 
+from bids2table._utils import parse_size
 from bids2table.schema import Schema
 
 TableBatch = Union[pd.DataFrame, pa.Table]
@@ -17,7 +20,7 @@ class ParquetWriter:
 
     Example::
 
-        with ParquetWriter("table.parquet") as writer:
+        with ParquetWriter("table") as writer:
             for batch in batches:
                 writer.write(batch)
 
@@ -38,86 +41,124 @@ class ParquetWriter:
 
     def __init__(
         self,
-        path: str,
+        prefix: str,
         coerce: bool = False,
-        buffer_size: int = 64 * 1024 * 1024,
+        buffer_size: Union[str, int] = "16 MiB",
+        partition_size: Union[str, int] = "64 MiB",
     ):
-        self.path = path
+        self.prefix = prefix
         self.coerce = coerce
+        if isinstance(buffer_size, str):
+            buffer_size = parse_size(buffer_size)
+        if isinstance(partition_size, str):
+            partition_size = parse_size(partition_size)
         self.buffer_size = buffer_size
-        self.schema: Optional[Schema] = None
+        self.partition_size = partition_size
+
+        self._schema: Optional[Schema] = None
         self._table: Optional[pa.Table] = None
         self._stream: Optional[pq.ParquetWriter] = None
-        # TODO: do something with these values
+        self._part = 0
         self._batch_count = 0
-        self._total_bytes_written = 0
-
-    def _init_stream(self, first_batch: TableBatch):
-        """
-        Initialize the stream, using the first batch to set the schema.
-        """
-        if self._stream is not None:
-            raise RuntimeError("parquet stream is already initialized")
-        if isinstance(first_batch, pd.DataFrame):
-            self.schema = Schema.from_pandas(first_batch)
-        else:
-            self.schema = Schema.from_pyarrow(table=first_batch)
-        # TODO: expose kwargs as config options
-        self._stream = pq.ParquetWriter(self.path, self.schema.to_pyarrow())
-
-    def close(self):
-        """
-        Close the stream.
-        """
-        if self._stream is not None:
-            self._stream.close()
+        self._bytes_written = 0
 
     def write(self, batch: TableBatch):
         """
-        Write a batch to the stream. If this is the first batch, the stream is
-        initialized.
+        Write a batch to the stream. If this is the first batch, the schema and stream
+        will be initialized.
         """
-        self._batch_count += 1
+        if self._schema is None:
+            self._init_schema(batch)
         if self._stream is None:
-            self._init_stream(batch)
+            self._init_stream()
         batch = self._to_pyarrow(batch)
         if self._table is None:
             self._table = batch
         else:
             self._table = pa.concat_tables([self._table, batch])
         if self._table.get_total_buffer_size() > self.buffer_size:
-            self.flush()
+            self._flush()
+        if self._bytes_written > self.partition_size:
+            self.close()
+        self._batch_count += 1
+
+    def _init_schema(self, first_batch: TableBatch):
+        """
+        Initialize the output schema from the first batch.
+        """
+        if isinstance(first_batch, pd.DataFrame):
+            self._schema = Schema.from_pandas(first_batch)
+        else:
+            self._schema = Schema.from_pyarrow(table=first_batch)
+
+    def _init_stream(self):
+        # TODO: expose kwargs as config options
+        self._stream = pq.ParquetWriter(
+            self._temp_path(), schema=self._schema.to_pyarrow()
+        )
+
+    def _path(self) -> str:
+        return f"{self.prefix}-{self._part:04d}.parquet"
+
+    def _temp_path(self) -> str:
+        path = Path(self.prefix)
+        temp_path = str(path.parent / f".tmp-{path.name}-{self._part:04d}.parquet")
+        return temp_path
 
     def _to_pyarrow(self, batch: TableBatch) -> pa.Table:
         """
         Convert a table batch to a PyArrow ``Table`` (if necessary).
         """
-        assert self.schema is not None, "writer schema not initialized"
         if isinstance(batch, pd.DataFrame):
-            if not self.schema.matches(batch):
+            if not self._schema.matches(batch):
                 logging.warning(
-                    "Parquet writer batch %d has a different schema\n\tpath: %s",
+                    "Parquet writer batch %d has a different schema\n\tprefix: %s",
                     self._batch_count,
-                    self.path,
+                    self.prefix,
                 )
                 if self.coerce:
-                    batch = self.schema.coerce(batch)
+                    batch = self._schema.coerce(batch)
             batch = pa.Table.from_pandas(batch)
-            batch = batch.replace_schema_metadata(self.schema.metadata)
+            batch = batch.replace_schema_metadata(self._schema.metadata)
         return batch
 
-    def flush(self):
+    def _flush(self):
         """
         Flush the internal buffer.
         """
         if self._table is not None:
-            self._total_bytes_written += self._table.get_total_buffer_size()
-            self._stream.write_table(self._table)
+            self._bytes_written += self._table.get_total_buffer_size()
+            logging.info(
+                "Flushing to partition\n"
+                f"\tpath: {self._path()}\n"
+                f"\tbatches: {self._batch_count}\n"
+                f"\tMB: {self._bytes_written / 1e6}"
+            )
+            self._stream.write_table(
+                self._table, row_group_size=self._table.get_total_buffer_size()
+            )
             self._table = None
+
+    def close(self):
+        """
+        Close the stream.
+        """
+        if self._stream is not None:
+            self._flush()
+            logging.info(
+                "Closing partition\n"
+                f"\tpath: {self._path()}\n"
+                f"\tbatches: {self._batch_count}\n"
+                f"\tMB: {self._bytes_written / 1e6}"
+            )
+            self._stream.close()
+            self._stream = None
+            os.rename(self._temp_path(), self._path())
+            self._bytes_written = 0
+            self._part += 1
 
     def __enter__(self) -> "ParquetWriter":
         return self
 
     def __exit__(self):
-        self.flush()
         self.close()
