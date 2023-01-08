8861aa556837fb63d454781f9a3693cc9992d5de
Connor Lane <connor.lane858@gmail.com>
2023-01-07 19:24:31 -0500

    update setup and add `requirements.txt`

--- setup.py
+++ setup.py
@@ -5,6 +5,32 @@ from setuptools import find_namespace_packages, find_packages, setup
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
+INSTALL_REQUIRES = [
+    "hydra-core",
+    "numpy",
+    "pandas",
+    "pyarrow",
+    "tabulate",
+    "pyyaml",
+]
+
+# NOTE: these dependencies also pinned in .pre-commit-config.yaml
+DEV_REQUIRES = [
+    "black==22.10.0",
+    "flake8==5.0.4",
+    "isort==5.10.1",
+    "mypy==0.982",
+    "pre-commit",
+    "pylint>=2.5.0",
+    "pytest",
+    "types-tabulate",
+    "types-PyYAML",
+]
+
+EXTRAS_REQUIRE = {
+    "dev": DEV_REQUIRES,
+}
+
 
 def get_version():
     path = HERE / "bids2table" / "__init__.py"
@@ -24,18 +50,15 @@ setup(
     author="Connor Lane",
     license="MIT",
     url="https://github.com/clane9/bids2table",
-    description="ETL BIDS data into tables",
+    description="Organize neuroimaging data derivatives into parquet tables",
     long_description=README,
     long_description_content_type="text/markdown",
     packages=packages,
     include_package_data=True,
     python_requires=">=3.7",
-    install_requires=[
-        "hydra-core",
-        "numpy",
-        "pandas>=1.2.0",
-        "pyarrow",
-        "tabulate",
-    ],
-    extras_require={},
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    entry_points={
+        "console_scripts": ["bids2table = bids2table:__main__._main"],
+    },
 )
